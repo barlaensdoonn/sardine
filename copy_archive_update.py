187e3c5637dd4d728776a167d11d763d2c5accda
barlaensdoonn <brandon.aleson@gmail.com>
2016-09-13 12:23:02 -0700

    first commit

---
+++ copy_archive_update.py
@@ -0,0 +1,274 @@
+#!/usr/local/bin/python3
+# combined file copier
+# 5/16/16
+
+import gspread
+import os
+import shutil
+from oauth2client.service_account import ServiceAccountCredentials
+
+src_dir = '/Users/baleson/Desktop/python_squeeze_me/exportsForLocalization'
+extra_path = 'Exports/localizedVP9'
+no_copy_dir = '/Users/baleson/Desktop/python_squeeze_me/no_copy'
+improper_dir = '/Users/baleson/Desktop/python_squeeze_me/improper'
+copied_dir = '/Users/baleson/Desktop/python_squeeze_me/copied'
+countries = ["AR", "AU", "BR", "DE", "FR", "IT", "MX", "NL", "PL", "QC", "RU", "UK"]
+
+archive_paths = {
+    "video_raid": '/Volumes/Video HD Raid 5/Allrecipes International Video Projects/editingLocalizing',
+    "video_raid_US": '/Volumes/Video HD Raid 5/AR US Videos ',
+    "video_localized": '/Volumes/Video_Localized/Localized',
+    "video_localized_US": '/Volumes/Video_Localized/US_videos',
+}
+
+zip_paths = {
+    'copy_zip': '/Volumes/Video HD Raid 5/Dropbox (Meredith)/ARCHIVE/archived',
+    'copy_zip_US': '/Volumes/Video HD Raid 5/Dropbox (Meredith)/ARCHIVE/archived_US',
+}
+
+country_paths = {
+    "AR": "/Volumes/public/International/Editorial/Video/Videos by country/AR/_New videos",
+    "AU": "/Volumes/public/International/Editorial/Video/Videos by country/AU/_New videos",
+    "BR": "/Volumes/public/International/Editorial/Video/Videos by country/BR/_New videos",
+    "DE": "/Volumes/public/International/Editorial/Video/Videos by country/DE/_New videos",
+    "FR": "/Users/baleson/Google Drive/Videos for review",
+    "IT": "/Volumes/public/International/Editorial/Video/Videos by country/IT/_New videos",
+    "MX": "/Volumes/public/International/Editorial/Video/Videos by country/MX/_New videos",
+    "NL": "/Volumes/public/International/Editorial/Video/Videos by country/NL/_New videos",
+    "QC": "/Volumes/public/International/Editorial/Video/Videos by country/QC/_New videos",
+    "PL": "/Volumes/Video HD Raid 5/Dropbox (Meredith)/PL",
+    "RU": "/Volumes/public/International/Editorial/Video/Videos by country/RU/_New videos",
+    "UK": "/Volumes/Video HD Raid 5/Dropbox (Meredith)/UK",
+}
+
+country_columns = {
+    "AR": 5,
+    "AU": 6,
+    "BR": 7,
+    "DE": 8,
+    "FR": 9,
+    "IT": 10,
+    "MX": 11,
+    "NL": 12,
+    "PL": 13,
+    "QC": 14,
+    "RU": 15,
+    "UK": 4
+}
+
+stats = {
+    "copy": [],
+    "copy_zip": [],
+    "copy_zip_US": [],
+    "not found": [],
+}
+
+
+def authenticate():
+    '''
+    authenticates account with Google drive
+    returns a tuple of the Pending tab and the archived by robot spreadsheet from the Master List
+    '''
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/baleson/Documents/credentials/googleAuth/spreadsheet-access-5092af4cc743.json', scope)
+
+    gc = gspread.authorize(credentials)
+
+    sheet = gc.open("Allrecipes Master Video List")
+    master_list_pending = sheet.get_worksheet(0)
+
+    return master_list_pending
+
+
+def replace_chars(string):
+    name = string
+
+    for x in [',', ':']:
+        name = name.replace(x, '')
+
+    for y in ['-', ' ']:
+        name = name.replace(y, '_')
+
+    return name
+
+
+def get_sheet_names(spreadsheet):
+    print('generating sheet_names dict...')
+
+    sheet_names = {}
+    video_names = spreadsheet.col_values(1)
+
+    for item in video_names:
+        if item != '':
+            if not item.isupper():
+                name = replace_chars(item)
+                sheet_names[name] = item
+
+    return sheet_names
+
+
+def update_sheet(spreadsheet, sheet_names, vid_name, country):
+    found_vid = spreadsheet.find(sheet_names[vid_name])
+    spreadsheet.update_cell(found_vid.row, country_columns[country], 'X')
+
+
+def clean_up(src_dir):
+    '''
+    cleans up the filenames list by removing .xmp files and .DS_Store.
+    also deletes .xmp files from directory
+    returns a tuple of the cleaned up filenames list, and
+    the src_files list of the full paths that point to the source files for copying
+    '''
+    for dirpath, dirnames, filenames in os.walk(src_dir):
+        for file in filenames:
+
+            if file == '.DS_Store':
+                filenames.remove(file)
+
+            if file[(len(file) - 4):len(file)] == ".xmp":
+                print("found a stray .xmp file, deleting: {}".format(file))
+                os.remove(os.path.join(dirpath, file))
+                filenames.remove(file)
+
+        src_files = [os.path.join(dirpath, file) for file in filenames]
+
+    return filenames, src_files
+
+
+def get_filenames(cleaned_file_list, src_file_list):
+    '''
+    returns a dict as {'file': ('vid_name', 'COUNTRY', 'src_file')}
+    where file is complete file name, such as 'Hot_cross_buns_UK.webm'
+    vid name is name of the video (which should be its directory too), such as 'Hot_cross_buns'
+    COUNTRY is 2 letter country for that file, such as 'UK'
+    src_file is full path to source file to use for copying
+    '''
+    vid_names = [file[:(len(file) - 8)] for file in cleaned_file_list]
+    vid_countries = [file[(len(file) - 7):(len(file) - 5)] for file in cleaned_file_list]
+    vid_split = list(zip(vid_names, vid_countries, src_file_list))
+
+    return {file: splits for (file, splits) in zip(cleaned_file_list, vid_split)}
+
+
+def find_archive_path(archive_paths, vid_name, file_name):
+    '''
+    scans through {archive_paths} to look for video directory, returns a tuple based on the results.
+
+    if no Exports/localizedVP9 directory exists, one is made
+    if it finds /vid_name/Exports/localized, returns dst_file with the full dir plus "copy" skip flag
+    - - if above is found on Video_Localized then flag is "copy_zip" or "copy_zip_US" depending on directory - -
+    if it doesn't find /vid_name anywhere, returns dst_file with no_copy_dir plus "not found" skip flag
+    '''
+    for key in archive_paths:
+        for dir in os.scandir(archive_paths[key]):
+            if dir.name == vid_name:
+                if not os.path.exists(os.path.join(dir.path, extra_path)):
+                    print("{} improperly set up for {}".format(extra_path, vid_name))
+                    print("creating {} directory".format(os.path.join(vid_name, extra_path)))
+                    os.mkdir(os.path.join(dir.path, extra_path))
+
+                if key == 'video_localized':
+                    flag = "copy_zip"
+                    backup_src = dir.path
+
+                elif key == 'video_localized_US':
+                    flag = "copy_zip_US"
+                    backup_src = dir.path
+
+                else:
+                    flag = "copy"
+                    backup_src = None
+
+                dst_file = os.path.join(dir.path, extra_path, file_name)
+
+                return dst_file, backup_src, flag
+
+                # else:
+                #     print("{} improperly set up for {} in {}".format(extra_path, vid_name, key))
+                #     flag = "improper"
+                #     backup_src = None
+                #     dst_file = os.path.join(improper_dir, file_name)
+                #     return dst_file, backup_src, flag
+
+    else:
+        print("didn't find {} anywhere".format(file_name))
+        flag = "not found"
+        backup_src = None
+        dst_file = os.path.join(no_copy_dir, file_name)
+
+        return dst_file, backup_src, flag
+
+
+def find_country_path(country, file_name):
+    if country in countries:
+        dst_file = os.path.join(country_paths[country], file_name)
+
+        return dst_file
+
+
+def copy(file_name, src_file, archive_path, backup_src, flag, country_path):
+
+    if flag[0:4] == 'copy':
+        if os.path.exists(archive_path):
+            print("{} already exists, removing from {}".format(file_name, "/".join(archive_path.split("/")[2:])))
+            os.remove(archive_path)
+        print("copying {} to {}\n".format(file_name, "/".join(archive_path.split("/")[2:])))
+        shutil.copy2(src_file, archive_path)
+
+        if os.path.exists(country_path):
+            print("{} already exists, removing from {}".format(file_name, "/".join(country_path.split("/")[2:])))
+            os.remove(country_path)
+        print("copying {} to {}\n".format(file_name, "/".join(country_path.split("/")[2:])))
+        shutil.copy2(src_file, country_path)
+        shutil.move(src_file, os.path.join(copied_dir, file_name))
+
+    else:
+        print("moving {} to {}\n".format(file_name, "/".join(archive_path.split("/")[2:])))
+        shutil.move(src_file, archive_path)
+
+    if backup_src:
+        backup_dst = os.path.join(zip_paths[flag], backup_src.split("/")[-1])
+        backup_dst_zip = backup_dst + '.zip'
+
+        if os.path.isfile(backup_dst_zip):
+            print('{} already exists, removing...'.format(backup_dst_zip))
+            os.remove(backup_dst_zip)
+        print('archiving {}\n'.format(backup_dst))
+        shutil.make_archive(backup_dst, 'zip', backup_src)
+
+    stats[flag].append(file_name)
+
+
+def printReport(stats):
+    print('{:*^50}'.format('SUMMARY'))
+
+    if all(x == [] for x in stats.values()):
+        print("nothing to do, i'm bored")
+
+    else:
+        for key in stats:
+            if any(stats[key]):
+                stats[key].sort()
+                print('{:-^40}'.format(key.upper()))
+                for item in stats[key]:
+                    print(item)
+
+
+if __name__ == '__main__':
+    spreadsheet = authenticate()
+    sheet_names = get_sheet_names(spreadsheet)
+
+    cleaned_lists = clean_up(src_dir)
+    vid_dict = get_filenames(*cleaned_lists)
+
+    for key in vid_dict:
+        file_name = key
+        vid_name = vid_dict[key][0]
+        country = vid_dict[key][1]
+        src_file = vid_dict[key][2]
+        archive_path, backup_src, task = find_archive_path(archive_paths, vid_name, file_name)
+        country_path = find_country_path(country, file_name)
+        copy(file_name, src_file, archive_path, backup_src, task, country_path)
+        update_sheet(spreadsheet, sheet_names, vid_name, country)
+
+    printReport(stats)
---
+++ gspread_archiver.py
@@ -0,0 +1,155 @@
+#!/usr/local/bin/python3
+# gspread archiver
+# 5/19/16
+
+import gspread
+import os
+import shutil
+from oauth2client.service_account import ServiceAccountCredentials
+
+localized = {'X'}
+localized_NA = {'X', 'N/A'}
+# localized_never = {'N/A'}
+
+source_paths = {
+    "US_videos": "/Volumes/Video HD Raid 5/AR US Videos ",
+    "localization": "/Volumes/Video HD Raid 5/Allrecipes International Video Projects/editingLocalizing",
+}
+
+archive_paths = {
+    'archive': '/Volumes/Video HD Raid 5/Dropbox (Meredith)/ARCHIVE/archived',
+    'archive_US': '/Volumes/Video HD Raid 5/Dropbox (Meredith)/ARCHIVE/archived_US',
+    'move': '/Volumes/Video_Localized/Localized',
+    'move_US': '/Volumes/Video_Localized/US_videos',
+}
+
+
+def authenticate():
+    '''
+    authenticates account with Google drive
+    returns a tuple of the Pending tab and the archived by robot spreadsheet from the Master List
+    '''
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/baleson/Documents/credentials/googleAuth/spreadsheet-access-5092af4cc743.json', scope)
+
+    gc = gspread.authorize(credentials)
+
+    sheet = gc.open("Allrecipes Master Video List")
+    master_list_pending = sheet.get_worksheet(0)
+    archived_by_robot = sheet.worksheet('archived by robot')
+
+    return master_list_pending, archived_by_robot
+
+
+def replace_chars(string):
+    name = string
+
+    for x in [',', ':']:
+        name = name.replace(x, '')
+
+    for y in ['-', ' ']:
+        name = name.replace(y, '_')
+
+    return name
+
+
+def get_localized(spreadsheet):
+    localized_videos = {}
+
+    for x in range(spreadsheet.row_count):
+        row_value = spreadsheet.row_values(x + 1)
+        row_value_set = set(row_value[3:15])
+        if row_value_set == localized or row_value_set == localized_NA:
+            name = replace_chars(row_value[0])
+            localized_videos[name] = x + 1
+
+    if any(localized_videos):
+        print("\nVIDEOS READY FOR ARCHIVING IN MASTER LIST:")
+
+        localized_list = list(localized_videos.keys())
+        localized_list.sort()
+        for video in localized_list:
+            print("{}".format(video))
+        print("\n")
+    else:
+        print("NO VIDEOS FOR ARCHIVING FOUND IN MASTER LIST")
+
+    return localized_videos
+
+
+def find_videos(paths, archive_paths, filename_dict):
+    '''
+    finds videos to archive on the computer based on videos found in the Master List
+    takes in filename_dict and creates video_dict with following format:
+    vid_name: src_path, archive_path, path_to_move_dir_to_on_Video_Localized_drive
+    '''
+    video_dict = {}
+
+    for key in source_paths:
+        for dir in os.scandir(source_paths[key]):
+            if dir.name in filename_dict:
+                if key == "US_videos":
+                    video_dict[dir.name] = dir.path, os.path.join(archive_paths['archive_US'], dir.name), os.path.join(archive_paths['move_US'], dir.name), filename_dict[dir.name]
+                else:
+                    video_dict[dir.name] = dir.path, os.path.join(archive_paths['archive'], dir.name), os.path.join(archive_paths['move'], dir.name), filename_dict[dir.name]
+
+    if any(video_dict):
+        print("FOUND THESE VIDEOS TO ARCHIVE ON THE COMPUTER:")
+
+        localized_list_02 = list(video_dict.keys())
+        localized_list_02.sort()
+        for video in localized_list_02:
+            print("{}".format(video))
+        print("\n")
+
+        if filename_dict.keys() == video_dict.keys():
+            print("ALL VIDEOS FROM MASTER LIST FOUND ON COMPUTER\n")
+        else:
+            print("THESE VIDEOS WERE NOT FOUND ON THE COMPUTER:")
+            print(set(filename_dict.keys()) - set(video_dict.keys()))
+            print("\n")
+
+    else:
+        print("DIDN'T FIND ANY VIDEOS TO ARCHIVE ON COMPUTER\n")
+
+    return video_dict
+
+
+def archive(video_dict, spreadsheets):
+    for key in video_dict:
+        src_file = video_dict[key][0]
+        archive_file = video_dict[key][1]
+        move_file = video_dict[key][2]
+        row_num = video_dict[key][3]
+        existing_archive = (archive_file + '.zip')
+
+        if os.path.exists(existing_archive):
+            print("{} archive already exists, removing...".format(key))
+            os.remove(existing_archive)
+        print("archiving {}...".format(key))
+        shutil.make_archive(archive_file, 'zip', src_file)
+
+        if os.path.exists(move_file):
+            print("{} already exists at destination, did not move".format(key))
+            moved = False
+        else:
+            print("moving {}...".format(key))
+            shutil.move(src_file, move_file)
+            moved = True
+
+        update_sheet(row_num, moved, spreadsheets)
+        print("done\n")
+
+
+def update_sheet(row_num, moved, spreadsheets):
+    values = spreadsheets[0].row_values(row_num)
+    if not moved:
+        values.append('*** not moved')
+    print("updating spreadsheet...")
+    spreadsheets[1].append_row(values)
+
+
+if __name__ == '__main__':
+    spreadsheets = authenticate()
+    video_dict = find_videos(source_paths, archive_paths, get_localized(spreadsheets[0]))
+    archive(video_dict, spreadsheets)
