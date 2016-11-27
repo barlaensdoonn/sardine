f7a3a7ee4103c89c86b26892348fc58cd2f51f41
barlaensdoonn <brandon.aleson@gmail.com>
2016-11-26 17:24:54 -0800

    code to copy file based on the end of its name (before the file extension like .png)

---
+++ suffix_file_copier.py
@@ -0,0 +1,23 @@
+# suffix file copier
+# 11/10/16
+
+import os
+import shutil
+
+src_dir = "/Volumes/Video_Localized"
+dst_dir = "/Volumes/public/International/Editorial/Video/Recipe Images/Raw Images"
+
+count = 0
+
+for dirpath, dirnames, filenames in os.walk(src_dir):
+    for filename in filenames:
+        if filename[len(filename) - 7:len(filename) - 4] == 'RAW':
+            try:
+                dst_file = os.path.join(dirpath, filename)
+                shutil.copy2(dst_file, dst_dir)
+                print("copied {}".format(filename))
+                count += 1
+            except PermissionError as e:
+                print("permission conflict with {}, skipping...".format(filename))
+
+print("copied {} files".format(count))
