3d430eceb72640b249b7464526c6eb529f53b41e
barlaensdoonn <brandon.aleson@gmail.com>
2017-02-05 23:06:09 -0800

    get plays and likes for all videos from vimeo API and write results to .csv

---
+++ vimeo/vimeo_get_stats.py
@@ -0,0 +1,58 @@
+#!/usr/local/bin/python3
+# get plays and likes for each video on vimeo from api
+# 2/5/17
+
+import csv
+import vimeo
+import vimeo_credents
+
+# api request goes here, while loop will step through pages until exhausted
+api_call = '/me/videos?sort=alphabetical'
+
+
+def call_api(caller, call):
+    next_page = call
+    vid_list = []
+    plays = 0
+    likes = 0
+
+    while next_page:
+        videos = caller.get(next_page)
+        data = videos.json()['data']
+
+        for i in range(len(data)):
+            play = data[i]['stats']['plays']
+            like = data[i]['metadata']['connections']['likes']['total']
+
+            plays += play
+            likes += like
+
+            tupled = (data[i]['name'], play, like)
+            vid_list.append(tupled)
+
+        next_page = videos.json()['paging']['next']
+
+    return (vid_list, plays, likes)
+
+
+def write_results(vid_list, plays, likes):
+    with open(vimeo_credents.stats_output_file, 'w') as out:
+        csv_out = csv.writer(out)
+        csv_out.writerow(['VIDEO', 'PLAYS', 'LIKES'])
+
+        for row in vid_list:
+            csv_out.writerow(row)
+
+        csv_out.writerow([])
+        csv_out.writerow(['TOTAL PLAYS', 'TOTAL LIKES'])
+        csv_out.writerow([plays, likes])
+
+
+if __name__ == '__main__':
+    vimeod = vimeo.VimeoClient(token=vimeo_credents.access_token, key=vimeo_credents.client_identifier, secret=vimeo_credents.client_secrets)
+
+    listed = call_api(vimeod, api_call)
+    write_results(*listed)
+
+    print('TOTAL PLAYS: {:,}'.format(listed[1]))
+    print('TOTAL LIKES: {:,}'.format(listed[2]))
