6486f00bec5410f56fc709340f48008940d157ec
barlaensdoonn <brandon.aleson@gmail.com>
2017-03-19 13:53:53 -0700

    change variable name for clarity

--- file_management/suffix_file_copier.py
+++ file_management/suffix_file_copier.py
@@ -13,8 +13,8 @@ for dirpath, dirnames, filenames in os.walk(src_dir):
     for filename in filenames:
         if filename[len(filename) - 7:len(filename) - 4] == 'RAW':
             try:
-                dst_file = os.path.join(dirpath, filename)
-                shutil.copy2(dst_file, dst_dir)
+                src_file = os.path.join(dirpath, filename)
+                shutil.copy2(src_file, dst_dir)
                 print("copied {}".format(filename))
                 count += 1
             except PermissionError as e:
