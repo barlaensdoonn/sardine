e28d97d96f2ffe64a338255e4e9b8589b0a8f4e7
barlaensdoonn <brandon.aleson@gmail.com>
2017-07-29 15:25:40 -0700

    get duration

--- vimeo/vimeo_get_stats.py
+++ vimeo/vimeo_get_stats.py
@@ -16,6 +16,7 @@ def call_api(caller, call):
     vid_list = []
     plays = 0
     likes = 0
+    total_duration = 0
 
     while next_page:
         videos = caller.get(next_page)
@@ -24,29 +25,31 @@ def call_api(caller, call):
         for i in range(len(data)):
             play = data[i]['stats']['plays']
             like = data[i]['metadata']['connections']['likes']['total']
+            duration = data[i]['duration']
 
             plays += play
             likes += like
+            total_duration += duration
 
-            tupled = (data[i]['name'], play, like)
+            tupled = (data[i]['name'], play, like, duration)
             vid_list.append(tupled)
 
         next_page = videos.json()['paging']['next']
 
-    return (vid_list, plays, likes)
+    return (vid_list, plays, likes, total_duration)
 
 
-def write_results(vid_list, plays, likes):
+def write_results(vid_list, plays, likes, total_duration):
     with open(vimeo_credents.stats_output_file, 'w') as out:
         csv_out = csv.writer(out)
-        csv_out.writerow(['VIDEO', 'PLAYS', 'LIKES'])
+        csv_out.writerow(['VIDEO', 'PLAYS', 'LIKES', 'DURATION [seconds]'])
 
         for row in vid_list:
             csv_out.writerow(row)
 
         csv_out.writerow([])
-        csv_out.writerow(['TOTAL PLAYS', 'TOTAL LIKES'])
-        csv_out.writerow([plays, likes])
+        csv_out.writerow(['TOTAL PLAYS', 'TOTAL LIKES', 'TOTAL DURATION [seconds]'])
+        csv_out.writerow([plays, likes, total_duration])
 
 
 if __name__ == '__main__':
@@ -57,3 +60,4 @@ if __name__ == '__main__':
 
     print('TOTAL PLAYS: {:,}'.format(listed[1]))
     print('TOTAL LIKES: {:,}'.format(listed[2]))
+    print('TOTAL DURATION: {:,}'.format(listed[3]))
