20c007614e4ccad9f1eb5f7dfc23a33f7071a1ce
Unknown <brandon.aleson@gmail.com>
2018-05-30 20:15:33 -0700

    capture both caas '$' ids and cms ids

--- CaaS/get_id_by_url.py
+++ CaaS/get_id_by_url.py
@@ -3,9 +3,9 @@
 # 5/25/18
 # updated 5/30/18
 
-import time
 import gspreadsheet
-from collections import Counter
+from time import sleep
+from collections import Counter, namedtuple
 from test_query import CaasClient
 
 
@@ -43,7 +43,44 @@ def update_sources(source_dict, source):
     return source_dict
 
 
+def _parse_ids(ids, response, key):
+    '''
+    type(ids) == set
+    if multiple ids found, return them separated by commas, otherwise just pop
+    the only element out of the set
+    '''
+    return ','.join(ids) if len(ids) > 1 else ids.pop()
+
+
+def get_cms_ids(response):
+    return {response[n]['cms_id'] for n in range(len(response)) if 'cms_id' in response[n].keys()}
+
+
+def get_caas_ids(response):
+    return {response[n]['$']['id'] for n in range(len(response)) if 'id' in response[n]['$'].keys()}
+
+
+def update_sheet(cms_ids, caas_ids, response):
+    for ids, key in zip([cms_ids, caas_ids], ['cms_id', 'caas_id']):
+        if ids:
+            id = _parse_ids(ids, response, key)
+            if id != 'None':
+                print('found {} {}'.format(key, id))
+                print('updating spreadsheet...')
+                content.update_cell(i, columns[key], id)
+        else:
+            print("didn't find any {} ids".format(key))
+
+
 if __name__ == '__main__':
+    Row = namedtuple('Row', ['source', 'url'])
+    columns = {
+        'source': 1,
+        'caas_id': 2,
+        'cms_id': 3,
+        'url': 5
+    }
+
     brands_not_in_caas = ['http://www.bhg.com/', 'https://www.marthastewart.com',
                           'https://www.fitnessmagazine.com/', 'https://www.shape.com/']
     caas_client = CaasClient()
@@ -54,26 +91,32 @@ if __name__ == '__main__':
     sheet_title = 'Hair Classification Training Corpus'
     hair = g.get_spreadsheet(title=sheet_title)
     content = g.get_worksheet(hair, 'Terms/Content')
-    sources = get_column_values(content, 'Content Site/Source')
-    urls = get_column_values(content, 'Content URL')
 
-    for entry in zip(sources, urls):
-        if entry[1].startswith('http'):
-            source = entry[0]
-            url = entry[1]
-
+    # iterate through the worksheet's rows
+    for i in range(1, content.row_count + 1):
+        row = Row(source=content.cell(i, columns['source']).value, url=content.cell(i, columns['url']).value)
+        if row.url.startswith('http'):
             # skip brands that are not currently in CaaS
-            if source in brands_not_in_caas:
+            if row.source in brands_not_in_caas:
                 continue
 
-            url_sources = update_sources(url_sources, source)
+            url_sources = update_sources(url_sources, row.source)
             search_totals['searched'] += 1
 
-            print('\nsearching for {}'.format(url))
-            elastic_search_request["query"]["constant_score"]["filter"]["term"]["web_article_url.raw"] = url
+            print('\nsearching for {}'.format(row.url))
+            elastic_search_request["query"]["constant_score"]["filter"]["term"]["web_article_url.raw"] = row.url
             response = caas_client.search(elastic_request=elastic_search_request)
 
             if response:
-                url_sources[source]['found'] += 1
+                url_sources[row.source]['found'] += 1
                 search_totals['found'] += 1
-            time.sleep(0.01)
+
+                # make a set of the cms id's from the entities in the response
+                cms_ids = get_cms_ids(response)
+                caas_ids = get_caas_ids(response)
+                update_sheet(cms_ids, caas_ids, response)
+
+            sleep(0.01)
+
+    print(url_sources)
+    print(search_totals)
