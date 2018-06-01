5c520d7b3ffa4db632fbc85f46503f96730472d1
Unknown <brandon.aleson@gmail.com>
2018-06-01 15:45:46 -0700

    represent a CaasRecord with a class with __slots__

--- CaaS/caas_query.py
+++ CaaS/caas_query.py
@@ -27,6 +27,22 @@ query_config_path = 'config/query_config.json'
 log_file = 'query.log'
 
 
+class CaaSRecord:
+    __slots__ = ('title', 'url', 'tags', 'caas_id', 'cms_id', 'gnlp_categories', 'wnlp_categories')
+
+    def __init__(self, title=None, url=None, tags=None, caas_id=None, cms_id=None, gnlp_categories=None, wnlp_categories=None):
+        self.title = title
+        self.url = url
+        self.tags = tags
+        self.caas_id = caas_id
+        self.cms_id = cms_id
+        self.gnlp_categories = gnlp_categories
+        self.wnlp_categories = wnlp_categories
+
+    def __repr__(self):
+        return f'Row({self.title}, {self.url}, {self.tags}, {self.caas_id}, {self.cms_id}, {self.gnlp_categories}, {self.wnlp_categories})'
+
+
 def _initialize_logger(name):
     logger = logging.getLogger(name)
     logger.info('{} logger instantiated'.format(name))
@@ -70,17 +86,30 @@ def capture_args():
         return _check_file(output)
 
 
-def extract_info(entry):
+def extract_caas_data(entry):
     # non-pronto data
     caas_id = rsp['$']['id']
+    cms_id = rsp['cms_id']
     taxonomy = rsp['si']['taxonomy']['tags']
     type = rsp['$type'][0]['$id'].split('/')[1]
     url = rsp['web_article_url']
     url_alt = rsp['asset_url']
     title = rsp['$name']
+    gnlp_id = rsp["$i_nlp_source_google"][0]['$id']
+    wnlp_id = rsp["$i_nlp_source_watson"]
 
+
+def extract_pronto_data(entry):
+    keys = {
+        'title': None,
+        'url': None,
+        'tag': None
+    }
     # pronto data
-    pronto_data = rsp['pronto']
+    pronto_data = entry['pronto']['article']
+
+
+
 
 
 if __name__ == '__main__':
