18ddecf558cd3a8e4bc830ef3871768a2303a722
Unknown <brandon.aleson@gmail.com>
2018-06-02 16:47:07 -0700

    add QueryData class

--- CaaS/caas_query.py
+++ CaaS/caas_query.py
@@ -26,21 +26,15 @@ elastic_path = 'config/elastic_search_request.json'
 query_config_path = 'config/query_config.json'
 log_file = 'query.log'
 
-
-class CaaSRecord:
-    __slots__ = ('title', 'url', 'tags', 'caas_id', 'cms_id', 'gnlp_categories', 'wnlp_categories')
-
-    def __init__(self, title='', url='', caas_id='', cms_id='', tags=[], gnlp_categories=None, wnlp_categories=None):
-        self.title = title
-        self.url = url
-        self.caas_id = caas_id
-        self.cms_id = cms_id
-        self.tags = tags
-        self.gnlp_categories = gnlp_categories
-        self.wnlp_categories = wnlp_categories
-
-    def __repr__(self):
-        return f'Row({self.title}, {self.url}, {self.tags}, {self.caas_id}, {self.cms_id}, {self.gnlp_categories}, {self.wnlp_categories})'
+record = {
+    'title': '',
+    'url': '',
+    'caas_id': '',
+    'cms_id': '',
+    'tags': [],
+    'gnlp_categories': [],
+    'wnlp_categories': []
+}
 
 
 def _initialize_logger(name):
@@ -64,7 +58,7 @@ def configure_logger():
 def _check_file(output):
     '''confirm overwrite of an existing file'''
     if os.path.isfile(output):
-        print('the specified output file already exists, do you want to overwrite it?')
+        logger.info('the specified output file already exists, do you want to overwrite it?')
         overwrite = input('y/n: ').lower()
         return output if overwrite.lower().startswith('y') else None
     else:
@@ -77,8 +71,8 @@ def capture_args():
     file is specified, check if it already exists and confirm overwrite if it does
     '''
     if len(sys.argv) > 2:
-        print('too many arguments')
-        print('usage: python3 query.py output_file.csv')
+        logger.error('too many arguments')
+        logger.info('usage: python3 query.py output_file.csv')
         raise SystemExit('output_file.csv is optional')
     elif len(sys.argv) == 2:
         output = sys.argv[1]
@@ -86,34 +80,57 @@ def capture_args():
         return _check_file(output)
 
 
-def extract_caas_data(entry):
-    # non-pronto data
-    caas_id = rsp['$']['id']
-    cms_id = rsp['cms_id']
-    taxonomy = rsp['si']['taxonomy']['tags']
-    type = rsp['$type'][0]['$id'].split('/')[1]
-    url = rsp['web_article_url']
-    url_alt = rsp['asset_url']
-    title = rsp['$name']
-    gnlp_id = rsp["$i_nlp_source_google"][0]['$id']
-    wnlp_id = rsp["$i_nlp_source_watson"]
+class QueryData:
 
+    def __init__(self, query_response):
+        self.response = query_response
+        self.records = {i: self._init_record() for i in range(len(self.response))}
+        self._extract_caas_data()
 
-def extract_pronto_data(entry):
-    keys = {
-        'title': None,
-        'url': None,
-        'tag': None
-    }
+    def _init_records(self):
+        return {
+            self.response[i]['$']['id']: {
+                'caas_id': self.response[i]['$']['id'],
+                'cms_id': '',
+                'title': '',
+                'url': '',
+                'brand': '',
+                'gnlp_id': '',
+                'wnlp_id': '',
+                'gnlp_categories': [],
+                'wnlp_categories': []
+            } for i in range(len(self.response))
+        }
 
-    # pronto data
-    pronto_data = entry['pronto']['article']
+    def _extract_caas_data(self):
+        '''
+        extract data from a caas record
+
+        some currently unextracted parameters:
+        taxonomy = entry['si']['taxonomy']['tags']  # si (sports illustrated) specific
+        type = entry['$type'][0]['$id'].split('/')[1]
+        url_alt = entry['asset_url']
+        pronto_data = entry['pronto']['article']
+        content = entry['web_article_content']
+        '''
+
+        for i in range(len(self.response)):
+            entry = self.response[i]
+            caas_id = entry['$']['id']
+            self.records[caas_id]['cms_id'] = entry['cms_id'] if 'cms_id' in entry.keys() else None
+            self.records[caas_id]['title'] = entry['web_article_title'] if 'web_article_title' in entry.keys() else entry['$name']
+            self.records[caas_id]['url'] = entry['web_article_url'] if 'web_article_url' in entry.keys() else None
+            self.records[caas_id]['brand'] = entry['brand'] if 'brand' in entry.keys() else None
+            self.records[caas_id]['gnlp_id'] = entry["$i_nlp_source_google"][0]['$id'] if "$i_nlp_source_google" in entry.keys() else None
+            self.records[caas_id]['wnlp_id'] = entry["$i_nlp_source_watson"] if "$i_nlp_source_watson" in entry.keys() else None
 
 
 if __name__ == '__main__':
     logger = configure_logger()
-    caas_client = client_wrapper.CaaSClient(elastic_path=elastic_path, query_config_path=query_config_path, logger=_initialize_logger('caas_client'))
     output_file = capture_args()
+    caas_client = client_wrapper.CaaSClient(elastic_path=elastic_path,
+                                            query_config_path=query_config_path,
+                                            logger=_initialize_logger('caas_client'))
 
     response = caas_client.search()
     rsp = response[0]
